8bd2ffa4a5847b82444d4011499a930ecee4e656
ashley <ashleyhsueh33@gmail.com>
2024-10-20 17:24:26 +0800

    :sparkles: (tailwind.config.js) add animation and keyframes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,15 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.5s ease forwards",
+      },
     },
   },
   plugins: [],
